7e79f640945c506870f3a4e8b959c55b72cee9f2
Edenik <Edenik5@gmail.com>
2020-10-13 14:41:15 +0300

    Creating Todo List Screen

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -6,11 +6,70 @@ class TodoListScreen extends StatefulWidget {
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
+  Widget _buildTask(int index) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('Task title'),
+            subtitle: Text('Pct 2, 2019 * High'),
+            trailing: Checkbox(
+              value: true,
+              onChanged: (value) {
+                print(value);
+              },
+              activeColor: Theme.of(context).primaryColor,
+            ),
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+      body: ListView.builder(
+        padding: EdgeInsets.symmetric(vertical: 80.0),
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'My Tasks',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    '1 of 10',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+            );
+          }
+          return _buildTask(index);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(Icons.add),
+        onPressed: () => print('Navigate to add task screen'),
       ),
     );
   }
